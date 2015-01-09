e8fd139f799ba2a57cc96fb3ec618e63aeb4efa8
Vladimir Enchev <vladimir.corsair@gmail.com>
2015-01-09 15:17:01 +0200

    Create init.js
    Added init file

---
+++ src/webber/init.js
@@ -0,0 +1,11 @@
+/**
+* @file implements Webber init
+* @author CORSAIR <vladimir.corsair@gmail.com>
+* @version 09.Jan.2015
+**/
+;(function($root, $dom) {
+    
+    $root.require.init({
+        baseUrl: './'
+    });
+})(window, document);
